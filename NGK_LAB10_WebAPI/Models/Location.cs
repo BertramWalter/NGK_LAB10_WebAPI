74c12caf2e1fac7c9cfceaced924acc8aed7f409
Henrik T. Jensen <HenrikThueJensen@gmail.com>
2019-11-11 10:14:25 +0100

    Added: Constraints to Location class

--- NGK_LAB10_WebAPI/Models/Location.cs
+++ NGK_LAB10_WebAPI/Models/Location.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace NGK_LAB10_WebAPI.Models
 {
     public class Location
     {
+        [Key]
         public string Name { get; set; }
+        [Required]
         public double Latitude { get; set; }
+        [Required]
         public double Longitude { get; set; }
 
     }
