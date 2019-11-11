69c45f21cbc1c735687716d42ea77075172c5a63
Bertram <bw-sp@live.dk>
2019-11-11 09:57:47 +0100

    Added AppDbContext class

---
+++ NGK_LAB10_WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NGK_LAB10_WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=Localhost;Initial Catalog=NGK3;Integrated Security=True");
+        }
+
+        //DbSets here:
+        //public DbSet<Model/tablename> Table { get; set; }
+    }
+}
