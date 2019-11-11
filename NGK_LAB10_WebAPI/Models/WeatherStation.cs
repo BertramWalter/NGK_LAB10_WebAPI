70f1942acc4d5ce3f4e79d861c576ea811c21aeb
FrandsAndersen <frands1995@gmail.com>
2019-11-11 10:17:46 +0100

    Added ID to WeatherStation

--- NGK_LAB10_WebAPI/Models/WeatherStation.cs
+++ NGK_LAB10_WebAPI/Models/WeatherStation.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
@@ -8,14 +10,18 @@ namespace NGK_LAB10_WebAPI.Models
 {
     public class WeatherStation
     {
+        public int WeatherStationId { get; set; }
 
         public DateTime Date { get; set; }
-        public int TemperatureC { get; set; }
+
+        [Column(TypeName = "decimal(10,1)")]
+        public decimal TemperatureC { get; set; }
 
         public Location Location { get; set; }
 
         public int Humidity { get; set; }
 
-        public int AirPressure { get; set; }
+        [Column(TypeName = "decimal(10,1)")]
+        public decimal AirPressure { get; set; }
     }
 }
