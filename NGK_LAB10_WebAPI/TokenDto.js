1416362a727e6f392245c5554c2eb4f0359d6094
Bertram <bw-sp@live.dk>
2019-12-02 09:37:36 +0100

    Added Test project

---
+++ NGK_LAB10_WebAPI/TokenDto.js
@@ -0,0 +1,8 @@
+// Find all classes with a name ending with Dto
+var TokenDto = /** @class */ (function () {
+    function TokenDto(token) {
+        this.token = token;
+    }
+    return TokenDto;
+}());
+//# sourceMappingURL=TokenDto.js.map
---
+++ Unit.Test.Controllers/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Unit.Test.Controllers
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
